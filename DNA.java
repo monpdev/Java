ce5847d85915318cce6a7c53ebf306fd3a5c5ab4
monpdev <35469743+monpdev@users.noreply.github.com>
2020-02-08 03:05:34 +0000

    Add files via upload

---
+++ DNA.java
@@ -0,0 +1,36 @@
+//DNA.java determines whether there is
+// a protein in a strand of DNA
+//Part of Java course/Codecademy
+
+public class DNA {
+
+  public static void main(String[] args) {
+ //  -. .-.   .-. .-.   .
+    //    \   \ /   \   \ / 
+    //   / \   \   / \   \  
+    //  ~   `-~ `-`   `-~ `-
+    String dna1 = "ATGCGATACGCTTGA";
+    String dna2 = "ATGCGATACGTGA";
+    String dna3 = "ATTAATATGTACTGA";
+    String dna = dna3;
+    
+    int length = dna.length();
+    System.out.println("Length: "+length);
+    
+    int start = dna.indexOf("ATG");        System.out.println("Start: "+start);
+    
+   int stop = dna.indexOf("TGA");
+    System.out.println("Stop: "+stop);
+    
+  if(start != -1 && stop != -1 && (stop-start)%3 == 0){
+    System.out.println("Condition 1,2 and 3 are satisfied.");
+    String protein = dna.substring(start,stop+3);
+    System.out.println("Protein: " + protein);
+}else {
+  System.out.println("No Protein.");    
+  }
+     
+    
+}
+
+}
